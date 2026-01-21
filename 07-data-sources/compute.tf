18736029750e658b0e4fb1180ea1e32c813e2214
Priyanka Vuddemari <58094868+priyanka-vuddemari@users.noreply.github.com>
2026-01-21 10:02:49 +0000

    added some more data sources

--- 07-data-sources/compute.tf
+++ 07-data-sources/compute.tf
@@ -18,6 +18,30 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+data "aws_vpc" "prod_vpc" {
+  tags = {
+    Env = "prod"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_iam_policy_document" "static_website_policy" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject", "s3:ListBucket"]
+    resources = ["${aws_s3_bucket.static_website.arn}/*"]
+  }
+}
+resource "aws_s3_bucket" "static_website" {
+  bucket = "my-static_bucket"
+}
 
 output "ubuntu_ami_data" {
   value = data.aws_ami.ubuntu
@@ -31,6 +55,21 @@ output "aws_region" {
   value = data.aws_region.current
 }
 
+output "aws_vpc_id" {
+  value = data.aws_vpc.prod_vpc.id
+}
+
+output "aws_availability_zones" {
+  value = data.aws_availability_zones.available.names
+}
+
+output "iam_policy"{
+  value = data.aws_iam_policy_document.static_website_policy.json
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.static_website.bucket
+}
 resource "aws_instance" "main_web" {
   //  ami retreived from data source
   ami                         = data.aws_ami.ubuntu.id
