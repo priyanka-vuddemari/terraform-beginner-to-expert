7b11767bf76b8891a536231f7f9a8e9a3e04649a
Priyanka Vuddemari <58094868+priyanka-vuddemari@users.noreply.github.com>
2026-01-21 09:17:21 +0000

    added data source config for ami

--- 07-data-sources/compute.tf
+++ 07-data-sources/compute.tf
@@ -1,11 +1,42 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+
+output "ubuntu_ami_data" {
+  value = data.aws_ami.ubuntu
+}
+
+output "aws_caller_identity" {
+  value = data.aws_caller_identity.current
+}
+
+output "aws_region" {
+  value = data.aws_region.current
+}
+
 resource "aws_instance" "main_web" {
-  # AMI ID for Amazon Linux 2 in us-east-1
-  ami = "ami-055744c75048d8296"
-  # AMI ID for NGNIX -Bitnami in us-east-1
-  # ami                         = "ami-01e8e2269458f4b3c"
+  //  ami retreived from data source
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   associate_public_ip_address = true
-  
+
   root_block_device {
     volume_size           = 10
     volume_type           = "gp3"
